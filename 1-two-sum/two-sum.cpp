5cebe45121d27dabcb404918f2d365a0f19cacfd
Tabassum Jilani Zari <tabassumzari336@gmail.com>
2026-09-13 15:32:49 +0530

    Time: 3 ms (67.41%) | Memory: 14.9 MB (44.62%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i<nums.size(); i++){
+            int required = target - nums[i];
+
+            if(mp.find(required) != mp.end()) {
+                return {mp[required], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
