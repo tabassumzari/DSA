fea930cc079da05c95372753eac13b75375df42d
Tabassum Jilani Zari <tabassumzari336@gmail.com>
2026-09-14 22:13:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (82.74%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        // find the pivot
+        int pivot = -1, n = nums.size();
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if (pivot == -1) {
+            reverse(nums.begin(), nums.end()); // in place changes
+            return;
+        }
+
+        // 2nd step: next larger element
+        for (int i = n - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+
+        // 3rd step : reverse (piv+1 to n-1)
+        // reverse(nums.begin() + pivot + 1, nums.end());
+
+        int i = pivot + 1, j = n - 1;
+        while (i <= j) {
+            swap(nums[i++], nums[j--]);
+        }
+    }
+};
