19a4ec3022a215dab901a90c762816e93201fd51
Tabassum Jilani Zari <tabassumzari336@gmail.com>
2026-09-16 17:18:05 +0530

    Time: 28 ms (38.93%) | Memory: 139.3 MB (26.59%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> res;
+
+        //1st window
+        for( int i = 0; i < k; i++) {
+            while(dq.size() > 0 && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+
+        for(int i=k; i<nums.size(); i++) {
+            res.push_back(nums[dq.front()]);
+
+            //remove not part of current window
+            while(dq.size() > 0 && dq.front() <= i-k) {
+                dq.pop_front();
+            }
+
+            //remove the smaller vals
+            while(dq.size() > 0 && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+        }
+
+        res.push_back(nums[dq.front()]);
+
+        return res;
+    }
+};
