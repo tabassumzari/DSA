7c7c04eb5896f651fdfb16f7e0a72472bb6ba527
Tabassum Jilani Zari <tabassumzari336@gmail.com>
2026-09-22 19:18:12 +0530

    Time: 42 ms (59.65%) | Memory: 54.5 MB (64.21%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,52 +1,90 @@
-class Solution {
+// class Solution {
+// public:
+//     int n;
+    
+//     bool isPalindrome(string &s, int l, int r) {
+        
+//         while(l < r) {
+//             if(s[l] != s[r])
+//                 return false;
+//             l++;
+//             r--;
+//         }
+        
+//         return true;
+        
+//     }
+    
+//     void backtrack(string &s, int idx, vector<string> curr, vector<vector<string>> &result) {
+        
+//         if(idx == n) {
+//             result.push_back(curr);
+//             return;
+//         }
+        
+        
+//         for(int i = idx; i<n; i++) {
+            
+//             if(isPalindrome(s, idx, i)) {
+                
+//                 curr.push_back(s.substr(idx, i-idx+1));
+                
+//                 backtrack(s, i+1, curr, result);
+                
+//                 curr.pop_back();
+                
+//             }
+            
+//         }
+        
+//     }
+    
+//     vector<vector<string>> partition(string s) {
+//         n = s.length();
+//         vector<vector<string>> result;
+//         vector<string> curr;
+        
+//         backtrack(s, 0, curr, result);
+        
+//         return result;
+        
+//     }
+// };
+
+
+
+//AC
+class Solution{
 public:
-    int n;
-    
-    bool isPalindrome(string &s, int l, int r) {
-        
-        while(l < r) {
-            if(s[l] != s[r])
-                return false;
-            l++;
-            r--;
-        }
-        
-        return true;
-        
+    bool isPalin(string s){
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        return s == s2; 
     }
-    
-    void backtrack(string &s, int idx, vector<string> curr, vector<vector<string>> &result) {
-        
-        if(idx == n) {
-            result.push_back(curr);
+
+    void getAllParts(string s, vector<string> &partitions, vector<vector<string>> &ans){
+        if(s.size()==0){
+            ans.push_back(partitions);
             return;
         }
-        
-        
-        for(int i = idx; i<n; i++) {
-            
-            if(isPalindrome(s, idx, i)) {
-                
-                curr.push_back(s.substr(idx, i-idx+1));
-                
-                backtrack(s, i+1, curr, result);
-                
-                curr.pop_back();
-                
+
+        for(int i=0; i<s.size(); i++) {
+            string part = s.substr(0, i+1);
+
+            if(isPalin(part)) { 
+                partitions.push_back(part);
+                getAllParts(s.substr(i+1), partitions, ans);
+                partitions.pop_back();
             }
-            
+
         }
-        
     }
-    
+
     vector<vector<string>> partition(string s) {
-        n = s.length();
-        vector<vector<string>> result;
-        vector<string> curr;
-        
-        backtrack(s, 0, curr, result);
-        
-        return result;
-        
+        vector<vector<string>> ans;
+        vector<string> partitions;
+
+        getAllParts(s,partitions,ans);
+        return ans;
     }
 };
