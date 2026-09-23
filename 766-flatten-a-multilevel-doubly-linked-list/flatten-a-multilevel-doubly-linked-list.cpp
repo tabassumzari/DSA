6069d86496e101ea0fc3662d4c67da1ecd4bfb6b
Tabassum Jilani Zari <tabassumzari336@gmail.com>
2026-09-24 01:30:49 +0530

    Time: 3 ms (63.95%) | Memory: 11.7 MB (43.86%) - LeetSync

---
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        if(head == NULL){
+            return head;
+        }
+
+        Node* curr = head;
+        while(curr!=NULL){
+            if(curr->child != NULL){
+                //flattem the child nodes
+                Node* next = curr->next;
+                curr->next = flatten(curr->child);
+
+                curr->next->prev = curr;
+                curr->child = NULL;
+
+                //find tail
+                while(curr->next != NULL){
+                    curr = curr->next;
+                }
+
+                //attach tail wit next ptr
+                if(next != NULL){
+                    curr->next = next;
+                    next->prev = curr;
+                }
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};
